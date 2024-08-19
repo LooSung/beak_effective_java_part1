29be54738124e317740f1de132343ae18be13fb8
LooTan_H <loosung@naver.com>
2024-08-19 11:20:39 +0900

    part.1 : 생성자 대신에 정적 팩터리 메서드를 고려하자
    - class안에 자신의 Instance로 만들면 다른 곳에서 사용하더라도
    - 하나의 Hashcode로 결과가 나오면서 자기 자신이 Control 할 수 있다.

--- src/main/java/chapter01/item01/Settings.java
+++ src/main/java/chapter01/item01/Settings.java
@@ -12,10 +12,12 @@ public class Settings {
 
     private Difficulty difficulty;
 
+    // private로 매서드를 생성하고
     private Settings() {}
 
+    // Get을 하는 Instance로 만들면 다른 곳에서 사용하더라도
+    // 하나의 Hashcode로 결과가 나오면서 자기 자신이 Control 할 수 있다.
     private static final Settings SETTINGS = new Settings();
-
     public static Settings getInstance() {
         return SETTINGS;
     }
